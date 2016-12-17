9621c4198bb465fca69b201cd9d99de76028ebd5
semo <semox78@gmail.com>
2016-12-17 21:32:01 +0100

    first commit

--- src/main/java/com/y4d3/services/CustomerServiceImpl.java
+++ src/main/java/com/y4d3/services/CustomerServiceImpl.java
@@ -50,6 +50,57 @@ public class CustomerServiceImpl implements CustomerService {
     private void loadAllCustomers() {
         customers = new HashMap<>();
 
+        Customer c1 = new Customer();
+        c1.setFirstname("Peter");
+        c1.setName("Venkman");
+        c1.setAddressOne("Mumbelstrasse 1");
+        c1.setAddress_two("Mumbelstrasse 12");
+        c1.setCity("2132");
+        c1.setZip("New York");
+        c1.setEmail("mail1@example.org");
+        c1.setPhonenumber("13251");
+        c1.setState("New York");
+
+        customers.put(1, c1);
+
+        Customer c2 = new Customer();
+        c2.setFirstname("Ray");
+        c2.setName("Stantz");
+        c2.setAddressOne("Mumbelstrasse 1");
+        c2.setAddress_two("Mumbelstrasse 12");
+        c2.setCity("2132");
+        c2.setZip("New York");
+        c2.setEmail("mail2@example.org");
+        c2.setPhonenumber("13251");
+        c2.setState("New York");
+
+        customers.put(2, c2);
+
+        Customer c3 = new Customer();
+        c3.setFirstname("Egon");
+        c3.setName("Spengler");
+        c3.setAddressOne("Mumbelstrasse 1");
+        c3.setAddress_two("Mumbelstrasse 12");
+        c3.setCity("2132");
+        c3.setZip("New York");
+        c3.setEmail("mail3@example.org");
+        c3.setPhonenumber("13251");
+        c3.setState("New York");
+
+        customers.put(3, c3);
+
+        Customer c4 = new Customer();
+        c4.setFirstname("Winston");
+        c4.setName("Zeddemore");
+        c4.setAddressOne("Mumbelstrasse 1");
+        c4.setAddress_two("Mumbelstrasse 12");
+        c4.setCity("2132");
+        c4.setZip("New York");
+        c4.setEmail("mail1@example.org");
+        c4.setPhonenumber("13251");
+        c4.setState("New York");
+
+        customers.put(4, c4);
 
     }
 }
